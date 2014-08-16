8c0a707c458534ce56b30ef1b6f196f7c2a44c01
nepstein <ne2210@columbia.edu>
2014-08-16 11:01:56 -0400

    html views

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
+app.engine('html', require('swig').renderfile);
+app.set('view engine', 'html');
 
 app.use(favicon());
 app.use(logger('dev'));
@@ -36,6 +38,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
+    swig.setDefaults({cache: false});
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
